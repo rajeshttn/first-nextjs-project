3a60c1c9e15b73be8aba586eb5cb7a94e7b29567
rajesh <rajesh@tothenew.com>
2024-05-21 15:01:31 +0530

    first commit

---
+++ app/blog-detail/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogDetail() {
+    return (
+      <main>
+           <h1> Blog Detail Page </h1>
+      </main>
+    );
+  }
---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+    return (
+      <main>
+           <h1> Blog List Page </h1>
+      </main>
+    );
+  }
